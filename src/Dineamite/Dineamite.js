27acd87c18229f6ce6ed8450407e564beba0da8c
Thomas Yoho <TYohoJr@Gmail.com>
2018-03-13 16:27:48 -0600

    fixed text going to edge of screen when window is small/mobile

--- src/Dineamite/Dineamite.js
+++ src/Dineamite/Dineamite.js
@@ -9,9 +9,9 @@ export default class Dineamite extends Component {
                 <a href="https://dine-amite.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Site</a>
                 <br />
                 <p>(use "test3" for both the username and password, or feel free to sign up!)</p>
-                <a href="https://github.com/TYohoJr/OurVerySpecialApp" target="_blank" rel="noopener noreferrer">Base Code</a>
+                <a href="https://github.com/TYohoJr/OurVerySpecialApp" target="_blank" rel="noopener noreferrer">Front-End Code</a>
                 <br />
-                <a href="https://github.com/TYohoJr/OurVerySpecialApp-Server" target="_blank" rel="noopener noreferrer">Server Code</a>
+                <a href="https://github.com/TYohoJr/OurVerySpecialApp-Server" target="_blank" rel="noopener noreferrer">Back-End Code</a>
                 <br />
                 <div id="dineamite-img" >
                     <img src={require("../project-images/Dineamite.png")} alt="Bike App" />
