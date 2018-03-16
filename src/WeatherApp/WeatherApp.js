c267c2b00137dae750b1d5e58869781fd85f4da9
Thomas Yoho <TYohoJr@Gmail.com>
2018-03-15 19:49:11 -0600

    addedsentence for clarity

--- src/WeatherApp/WeatherApp.js
+++ src/WeatherApp/WeatherApp.js
@@ -6,7 +6,7 @@ export default class WeatherApp extends Component {
         return (
             <div id="weatheapp-div" className="project-div">
                 <div className="project-info">
-                    <p>I created weather app to get a local forecast without all the ads and bloatware. You enter your 5 digit zip code into the input field. 
+                    <p>This was the first app i created on my own. I created weather app to get a local forecast without all the ads and bloatware. You enter your 5 digit zip code into the input field. 
                         You then get a table with a current weather report, as well as corresponding images. Beneath that is a table with a short forecast.</p>
                     <a href="https://my-weather-app2.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Site</a>
                     <br />
