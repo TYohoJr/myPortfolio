a665849f907b7b43d0e16c9c539a9b937f90ea34
Thomas Yoho <TYohoJr@Gmail.com>
2018-05-01 11:04:10 -0500

    started food log

--- src/FoodLog/FoodLog.js
+++ src/FoodLog/FoodLog.js
@@ -6,15 +6,18 @@ export default class FoodLog extends Component {
         return (
             <div id="foodlog-div" className="project-div">
                 <div className="project-info">
-                    <p>Food Log info</p>
+                    <p>Food Log was created to replicate food tracking apps, such as MyFitnessPal, but with less extra features that i'm never going to use. As well as having no advertisements without the need to pay for a subscription.
+                        The front end is built using React and Redux, the back end is built from Express, and the data is being managed through a postgreSQL database. The app also makes use of axios for http calls and the Nutrionix API.
+                        Security is maintained using Bcrypt and JsonWebTokens.
+                    </p>
                     <a href="https://food-log1.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Site</a>
                     <br />
-                    <p>(use "(placeholder)" for the username and "(placeholder)" for the password, or feel free to sign up!)</p>
+                    <p>(use "test1" for the username and "test1" for the password, or feel free to sign up!)</p>
                     <a href="https://github.com/TYohoJr/food-log" target="_blank" rel="noopener noreferrer">Front-End Code</a>
                     <br />
                     <a href="https://github.com/TYohoJr/food-log-server" target="_blank" rel="noopener noreferrer">Back-End Code</a>
                 </div>
-                <img className="project-image" src={require("../project-images/placeholder.png")} alt="Food Log" />
+                <img className="project-image" src={require("../project-images/FoodLog.png")} alt="Food Log" />
             </div>
         )
     }
--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -57,18 +57,19 @@ export default class Navbar2 extends React.Component {
                   <DropdownItem onClick={this.props.showBakerySite}>
                     Bakery Site
                   </DropdownItem>
+                   <DropdownItem onClick={this.props.showFoodLog}>
+                    Food Log
+                  </DropdownItem>
                   <DropdownItem onClick={this.props.showWeatherApp}>
                     Weather App
                   </DropdownItem>
-                  <DropdownItem onClick={this.props.showBikeApp}>
+                  {/* <DropdownItem onClick={this.props.showBikeApp}>
                     Bike App
-                  </DropdownItem>
+                  </DropdownItem> */}
                   {/* <DropdownItem onClick={this.props.showGroceryList}>
                     Grocery List
                   </DropdownItem> */}
-                  {/* <DropdownItem onClick={this.props.showFoodLog}>
-                    Food Log
-                  </DropdownItem> */}
+                 
                 </DropdownMenu>
               </UncontrolledDropdown>
             </Nav>
