a0cd4ed72ebb9c220800cc37db0bfcae82912092
Thomas Yoho <TYohoJr@Gmail.com>
2018-05-14 09:46:33 -0500

    hyperlink to code school

--- src/aboutme/aboutme.js
+++ src/aboutme/aboutme.js
@@ -10,7 +10,7 @@ export default class Aboutme extends Component {
                 <p id="about-me-txt">I'm 24 years old and live in Naperville, IL.<br />
                     Im an aspiring programmer looking for the right company to start as a junior dev.<br />
                     {/* I completed Montana Code School in 2018.<br /> */}
-                    I completed Montana Code School in April 2018.<br />
+                    I completed <a href="https://montanacodeschool.com/" target="_blank" rel="noopener noreferrer">Montana Code School</a> in April 2018.<br />
                     I have a fundamental knowledge of: <br />JavaScript, HTML, CSS, React, Redux, Express, NodeJs, Axios, MongoDB, postgreSQL, Git/GitHub and various API's<br />
                     My projects in the dropdown above make use of these technologies (inclding the site you're currently on)<br/>
                     Visit my LinkedIn profile to contact me</p>
