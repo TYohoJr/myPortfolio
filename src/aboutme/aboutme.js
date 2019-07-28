d1c840d085637385095cf77ecb9ebd380801b0ed
Thomas Yoho <TYohoJr@Gmail.com>
2019-07-28 10:13:42 -0500

    updated about me

--- src/aboutme/aboutme.js
+++ src/aboutme/aboutme.js
@@ -7,11 +7,12 @@ export default class Aboutme extends Component {
             <div id="about-me-div">
                 <h1 id="about-me-header">Hello! My name is Thomas Yoho <small><sup>:)</sup></small></h1>
                 <img id="about-me-pic" src={require("../project-images/fb-pic.jpg")} alt="me" />
-                <p id="about-me-txt">I'm 25 years old and live in Naperville, IL.<br />
-                    Im an aspiring programmer looking for the right opportunity to start my career.<br />
+                <p id="about-me-txt">I'm 25 years old and live in Schaumburg, IL.<br />
+                    I'm a Web Developer who's always looking for the next challenge in my career.<br />
                     {/* I completed Montana Code School in 2018.<br /> */}
-                    I completed <a href="https://montanacodeschool.com/" target="_blank" rel="noopener noreferrer">Montana Code School</a> in April 2018.<br />
-                    I have a fundamental knowledge of: <br />JavaScript, HTML, CSS, React, Redux, Express, NodeJs, Axios, MongoDB, postgreSQL, Git/GitHub and various API's<br />
+                    I completed <a href="https://montanacodeschool.com/" target="_blank" rel="noopener noreferrer">Montana Code School</a> in April 2018.<br/>
+                    I have professional experience working on a team with other developers as well as with clients.<br /> <br/>
+                    I have a fundamental knowledge of: <br />JavaScript, Angular, React, SQL Server, Node, C#, .Net, HTML, CSS, Redux, Express, Axios, MongoDB, postgreSQL, Git/GitHub/BitBucket and more.<br /> <br/>
                     My projects in the dropdown above make use of these technologies (including the site you're currently on)<br/>
                     Visit my LinkedIn profile to contact me</p>
             </div>
